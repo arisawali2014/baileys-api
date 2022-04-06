2c6462c63e16c949ad992cde4f87f03898416160
Ari Sawali <arisawali2020@gmail.com>
2022-04-06 16:40:23 +0700

    .

--- app.js
+++ app.js
@@ -4,7 +4,18 @@ import nodeCleanup from 'node-cleanup'
 import routes from './routes.js'
 import { init, cleanup } from './whatsapp.js'
 
+import cors from 'cors';
+import { createServer } from 'http';
+import { Server } from 'socket.io'; //replaces (import socketIo from 'socket.io')
+import handleSocket from './services/socket.js';
+
 const app = express()
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, { cors: { origin: '*' } });
+
+io.on('connection',handleSocket);
+
 const host = process.env.HOST ?? '127.0.0.1'
 const port = parseInt(process.env.PORT ?? 8000)
 
@@ -12,7 +23,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use('/', routes)
 
-app.listen(port, host, () => {
+httpServer.listen(port, host, () => {
     init()
     console.log(`Server is listening on http://${host}:${port}`)
 })
